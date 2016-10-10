c0cf4fd28e5550b4433ff3e7bf05bbbdfeaa01e6
makalaaneesh <makalaaneesh@yahoo.com>
2016-10-10 19:52:16 +0530

    base functions for algo ready

--- algo.py
+++ algo.py
@@ -1,10 +1,12 @@
 import nlp
+import metrics
 
 
 class Cluster:
 	def __init__(self):
 		self.comments = []
 		self.terms = {}
+		self.center = {}
 
 	def add_comment(self, comment):
 		index = len(self.comments)
@@ -23,7 +25,56 @@ class Cluster:
 				continue
 			if term not in self.terms.keys():
 				self.terms[term] = set()
-			self.terms[term].add(index)
+				self.center[term] = 0
+			self.terms[term].add(comment)
+			self.center[term] += 1
+
+	def remove_comment(self, comment):
+		if comment not in self.comments:
+			print "Comment does not exist in cluster"
+			return
+		self.comments.remove(comment)
+		for term in self.terms.keys():
+			try:
+				# print term
+				self.terms[term].remove(comment)
+				self.center[term] -= 1
+			except (ValueError, KeyError):
+				pass
+
+
+	def get_distance_from_center(self,comment):
+		comment = nlp.preprocess(comment)
+		sim = 0
+		term_vector = set(metrics.get_term_vector(comment))
+		center_term_vector = set(self.center.keys())
+		intersection = term_vector & center_term_vector
+		for term in intersection:
+			sim = sim + min(self.center[term],2)
+		sim = float(sim)
+		# print "sim 1", sim
+		if sim > metrics.T:
+			sim = float(1)
+		else:
+			sim = sim/float(metrics.T)
+		# print "sim 2",sim
+
+		if sim == float(0):
+			return float("inf")
+		else:
+			return (1.0/sim) - 1.0
+
+
+	def get_radius(self):
+		max_dist = float("-inf")
+		for comment in self.comments:
+			distance = self.get_distance_from_center(comment)
+			if distance > max_dist:
+				max_dist = distance
+		return max_dist
+
+	def get_center(self):
+		return self.center
 
 	def print_terms(self):
 		print self.terms
@@ -35,5 +86,8 @@ class Cluster:
 
 if __name__ == "__main__":
 	c = Cluster()
-	c.add_comment("This is a comment")
-	c.print_terms()
+	c.add_comment("This is a cooooomment")
+	before = c.terms
+	c.add_comment("i love lady gaga")
+	c.remove_comment(c.comments[1])
+	after = c.terms
---
+++ metrics.py
@@ -0,0 +1,44 @@
+import nlp
+
+# constants
+D = 2
+T = 15
+
+def get_term_vector(comment):
+	terms = []
+	for i in range(1,4):
+		# for 1,2,3
+		iterms = nlp.extract_ngrams(comment, i)
+		terms  = terms + iterms
+	# print terms
+	terms = [term for term in terms if len(nlp.remove_stopwords(term)) > 0]
+	return terms
+
+
+def comment_comment_similarity(comment1, comment2):
+	comment1 = nlp.preprocess(comment1)
+	comment2 = nlp.preprocess(comment2)
+	term_vector1 = set(get_term_vector(comment1))
+	term_vector2 = set(get_term_vector(comment2))
+	intersection = term_vector1 & term_vector2
+	sim = float(len(intersection))
+	if sim > D:
+		val = 1
+	else:
+		val = sim/float(D)
+	return val
+
+
+def comment_comment_distance(comment1, commment2):
+	sim = comment_comment_similarity(comment1, comment2)
+	if sim == float(0):
+		return float("inf")
+	else:
+		return (1.0/sim) - 1.0
+
+
+
+
+
+print get_term_vector("THis is a comment")
+comment_comment_similarity("This is a comment", "Hey I am a comment")
