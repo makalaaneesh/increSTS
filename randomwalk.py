d83acc1b256af4262c6057612eece063b8ca2a91
acemaster <vivekhtc25@gmail.com>
2016-11-21 08:55:29 +0530

    Merge conflicts fixed and added threaded approach

--- randomwalk.py
+++ randomwalk.py
@@ -8,6 +8,7 @@ import numpy as np
 import pdb
 # import stringcmp
 from collections import OrderedDict
+import threading
 
 """
 ============================
@@ -93,7 +94,7 @@ def getallngrams():
 	"""
 	Getting all the ngrams
 	"""
-	f = open("cleanedcomments.txt", "r")
+	f = open("commentstest.txt", "r")
 	x = f.read()
 	ngrams_list=[]
 	for line in x.split("\n"):
@@ -324,6 +325,145 @@ def randomwalk(B,X,Y):
 					final_word_map[str(node_list[node_index])].append((str(node_list[row_array[word_index]]),cost_matrix[node_index,row_array[word_index]]))
 	print final_word_map
 
+def init_randomwalk(B):
+	A = nx.to_numpy_matrix(B)
+	print A
+	node_list = B.nodes()
+	node_len = len(node_list)
+	for i in range(0,len(node_list)):
+		total = 0.0
+		for j in range(0,len(node_list)):
+			total = total + A[i,j]
+		if total!=0:
+			for j in range(0,len(node_list)):
+				A[i,j] = A[i,j]/total
+	print A
+	#Random walks algorithm
+	A_mask = np.ma.masked_where(A==0., A)
+	P = []
+	for i in range(1,STEPS_VALUE+1,2):
+		P.append(np.linalg.matrix_power(A,i))
+	return A,A_mask,node_list,P
+
+
+def randomwalk_thread(B,node_arr):
+	"""Random walk threading function
+	
+	Threaded approach to random walk to improve speed
+	
+	Arguments:
+		B {Graph} -- Starting graph
+	"""
+	#Create weight probabities:
+	A,A_mask,node_list,P_arr = init_randomwalk(B)
+	random_walk_threads = []
+	final_word_map = {}
+	for node_index in node_arr:
+		if type(node_list[node_index]) is WordNode and node_list[node_index].isNoisy:
+			r = RandomWalk(node_index,A,A_mask,node_list,P_arr,final_word_map)
+			r.start()
+			random_walk_threads.append(r)
+	print "**********Total threads : "+ str(len(random_walk_threads))
+	for r in random_walk_threads:
+		r.join()
+
+class RandomWalk(threading.Thread):
+	"""docstring for Client"""
+	def __init__(self,node_index,A,A_mask,node_list,P_arr,final_word_map):
+		threading.Thread.__init__(self)
+		# self.private_key = private_key
+		self.node_index = node_index
+		self.A = A
+		self.A_mask = A_mask
+		self.node_list = node_list
+		self.P_arr = P_arr
+		self.final_word_map = final_word_map
+		# print "Thread id : "+ str(self.thread_count)
+
+	def run(self):
+		print "Created Random walk thread"
+		node_len = len(self.node_list)
+		hit_matrix = np.zeros(node_len)
+		r_matrix = np.zeros(node_len)
+		norm_matrix = np.zeros(node_len)
+		cost_matrix = np.zeros(node_len)
+		node_index = self.node_index
+		start_node_index = node_index
+		source_node_index = node_index
+		for i in range(0,(STEPS_VALUE/2)+1):
+			start_node_index = node_index
+			source_node_index = node_index
+			P = self.P_arr[i]
+			P = np.ma.masked_array(P, self.A_mask.mask)
+			np.ma.set_fill_value(P, 0.)
+			P = P.filled()
+			hits = 0
+			print "STEP "+str(i)
+			print self.node_list[start_node_index]
+			while (type(self.node_list[source_node_index]) is ContextNode) or (type(self.node_list[source_node_index]) is WordNode and self.node_list[source_node_index].isNoisy) or (hits < MAX_HITS):
+				hits = hits + 1
+				row_array = P[source_node_index,None,:]
+				row_array[0,start_node_index]=0
+				source_node_index = np.argmax(row_array)
+				if row_array[0,source_node_index] == 0:
+					print "No where to go"
+					break
+				print "->"
+				print self.node_list[source_node_index]
+				# pdb.set_trace()
+				if (type(self.node_list[source_node_index]) is WordNode and not self.node_list[source_node_index].isNoisy) or (hits >= MAX_HITS):
+					break
+			print "STEP Done"
+			r_matrix[source_node_index]=r_matrix[source_node_index]+1
+			hit_matrix[source_node_index]=hits
+		H_matrix = np.true_divide(hit_matrix,r_matrix)
+		where_are_NaNs = np.isnan(H_matrix)
+		H_matrix[where_are_NaNs] = 0.
+		print "==========Final H Matrix==========="
+		print H_matrix
+		total = 0.0
+		for j in range(0,len(self.node_list)):
+			total = total + H_matrix[j]
+		if total!=0:
+			for j in range(0,len(self.node_list)):
+				H_matrix[j] = H_matrix[j]/total
+		print H_matrix
+		final_word_map={}
+		word_node_list = []
+
+		#Getting only the word node indices
+		for i in range(0,len(self.node_list)):
+			if type(self.node_list[i]) is WordNode:
+				word_node_list.append(i)
+
+		#Building the cost matrix
+		for j in word_node_list:
+			if node_index != j:
+				n = str(self.node_list[node_index])
+				m = str(self.node_list[j])
+				lcs_n_m = len(nlp.lcs(n,m))
+				max_length = max(len(n),len(m))
+				lcsr_n_m = float(lcs_n_m)/float(max_length)
+				edit_n_m = nlp.editex("".join(OrderedDict.fromkeys(n)),"".join(OrderedDict.fromkeys(m)))
+				if edit_n_m !=0:
+					sim_cost_n_m = lcsr_n_m/edit_n_m
+					print sim_cost_n_m
+					cost_matrix[j] = float(H_matrix[j] + sim_cost_n_m)
+					# print "="
+					# print cost_matrix[i,j]
+		print "===========Final Cost Matrix================"
+		print cost_matrix
+		#Cost matrix done
+		if type(self.node_list[node_index]) is WordNode and self.node_list[node_index].isNoisy:
+			self.final_word_map[str(self.node_list[node_index])]=[]
+			# pdb.set_trace()
+			row_array = cost_matrix
+			row_array = np.asarray(np.argsort(row_array)).reshape(-1)[::-1]
+			for word_index in range(0,MAX_WORDS):
+				if type(self.node_list[row_array[word_index]]) is WordNode and not self.node_list[row_array[word_index]].isNoisy:
+					self.final_word_map[str(self.node_list[node_index])].append((str(self.node_list[row_array[word_index]]),cost_matrix[row_array[word_index]]))
+		print self.final_word_map
+
 
 
 if __name__ == "__main__":
@@ -331,6 +471,8 @@ if __name__ == "__main__":
 	list_of_ngrams=getallngrams()
 	print list_of_ngrams
 	B,X,Y=constructGraph(list_of_ngrams,5)
-	randomwalk(B,X,Y)
+	indexes = [i for i in range(0,len(B.nodes()))]
+	# randomwalk(B,X,Y)
+	randomwalk_thread(B,indexes)
 
 
