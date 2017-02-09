38dccd75a5372cd80b5e0bfe593b6bdfee868bf9
makalaaneesh <makalaaneesh@yahoo.com>
2017-02-09 15:24:21 +0530

    hashed and removed collect dependency

--- graphx.py
+++ graphx.py
@@ -7,7 +7,10 @@ spark-submit --packages graphframes:graphframes:0.2.0-spark2.0-s_2.11 graphx.py 
 from pyspark import SparkContext
 from pyspark.sql import *
 from pyspark.sql.functions import monotonically_increasing_id
+from pyspark.sql.functions import udf, col
+from pyspark.sql.types import *
 import sys
+import hashlib
 from operator import add
 
 THRESHOLD_COUNT = 6
@@ -16,48 +19,67 @@ sc = SparkContext()
 sqlContext = SQLContext(sc)
 #Getting Word count map to use
 lines = sqlContext.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
-counts = lines.flatMap(lambda x: x.split(' ')).map(lambda x: (x, 1)).reduceByKey(add).collectAsMap()
+countsrdd = lines.flatMap(lambda x: x.split(' ')).map(lambda x: (x, 1)).reduceByKey(add)
+# counts = countsrdd.collectAsMap()
+
+
+def hashed(str):
+	return hashlib.sha256(str).hexdigest()
 
 
 def parsevertices(r):
 	r = r.split("|")
 	r[0] = str(r[0])
 	r[1] = str(r[1])
-	word_count = 0
-	try:
-		word_count = counts[r[0]]
-	except:
-		word_count = 0
-	word_type = WORD_TYPES[0]
-	if word_count < THRESHOLD_COUNT:
-		word_type = WORD_TYPES[1]
-	final_list = [(r[0],word_type), (r[1],"context")]
+	# word_count = 0
+	# try:
+	# 	word_count = counts[r[0]]
+	# except:
+	# 	word_count = 0
+	# word_type = WORD_TYPES[0]
+	# if word_count < THRESHOLD_COUNT:
+	# 	word_type = WORD_TYPES[1]
+	final_list = [(hashed(r[0]), r[0],"word"), (hashed(r[1]), r[1],"context")]
 	return final_list
 
-def parseedges(r, vertices_df):
+def parseedges(r):
 	r = r.split("|")
 	r[0] = str(r[0])
 	r[1] = str(r[1])
-	# index = [0,0]
-	# index[0] = vertices_df.rdd.filter(lambda x: x["value"] == r[0]).collect()[0]["id"]
-	# index[1] = vertices_df.rdd.filter(lambda x: x["value"] == r[1]).collect()[0]["id"]
-	# print index
-
-	final_list = [(vertices_df_dictionary[r[0]], vertices_df_dictionary[r[1]], 1)]
-	# print final_list
+	# final_list = [(vertices_df_dictionary[r[0]], vertices_df_dictionary[r[1]], 1)]
+	final_list = [(hashed(r[0]), hashed(r[1]), 1)]
 	return final_list
 
 
 edge_list_file = sqlContext.read.text("edgelist").rdd.map(lambda r: r[0])
-vertices = edge_list_file.flatMap(lambda r: parsevertices(r)) # returns an RDD list of (vertex, type) by spitting the input line
+vertices = edge_list_file.flatMap(lambda r: parsevertices(r)) # returns an RDD list of (id, vertex, type) by spitting the input line
 
-vertices_df = sqlContext.createDataFrame(vertices,["value", "type"]).dropDuplicates()
-vertices_df = vertices_df.withColumn("id", monotonically_increasing_id())
+vertices_df = sqlContext.createDataFrame(vertices,["id","value", "type_initial"]).dropDuplicates()
 
-vertices_df_dictionary = vertices_df.rdd.flatMap(lambda r: {r["value"]: r["id"]}.items())
-vertices_df_dictionary = dict(vertices_df_dictionary.collect()) # creates the dictionary to search the ids of vertices while adding to the edges dataframe
 
-edges = edge_list_file.flatMap(lambda r: parseedges(r, vertices_df_dictionary)) #creates an rdd list of (src,dst, weight)
+
+def get_word_type(_type, count):
+	if _type == "context":
+		return _type
+	if count < THRESHOLD_COUNT:
+		return "OOV"
+	else:
+		return "IV"
+
+
+counts_df = sqlContext.createDataFrame(countsrdd, ["value", "count"])
+vertices_counts = vertices_df.join(counts_df, vertices_df.value == counts_df.value, "left_outer")
+word_type_function = udf(get_word_type, StringType())
+vertices_counts = vertices_counts.withColumn("type", word_type_function("type_initial","count")).select("id", vertices_df["value"], "type")
+vertices_counts.filter(vertices_counts.type != "context").show(n =100)
+print "no of rows are ",vertices_counts.count()
+
+
+# vertices_df = vertices_df.withColumn("id", monotonically_increasing_id())
+# vertices_df_dictionary = vertices_df.rdd.flatMap(lambda r: {r["value"]: r["id"]}.items())
+# vertices_df_dictionary = dict(vertices_df_dictionary.collect()) # creates the dictionary to search the ids of vertices while adding to the edges dataframe
+
+edges = edge_list_file.flatMap(lambda r: parseedges(r)) #creates an rdd list of (src,dst, weight)
 edges_df = sqlContext.createDataFrame(edges,["src", "dst", "weight"])
 
 edges_df = edges_df.groupBy(["src", "dst"]).sum("weight") #aggregating the weights
@@ -65,12 +87,14 @@ edges_df = edges_df.withColumnRenamed("sum(weight)", "weight")
 
 edges_df.show()
 vertices_df.show()
+print "no of rows are ", vertices_df.count()
+
 
 from graphframes import *
-g = GraphFrame(vertices_df, edges_df)
+g = GraphFrame(vertices_counts, edges_df)
 g1 = g.inDegrees
-g1 = g1.filter(g1.inDegree == 2)
-g1.show()
+# g1 = g1.filter(g1.inDegree == 2)
+g1.groupBy("inDegree").count().show()
 print g1.count()
 
 
